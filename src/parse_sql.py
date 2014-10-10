da12d71ba36eab45e3bf83285112fbbc9e152926
Yashodhan Karandikar <yashkarandikar@gmail.com>
2014-10-10 00:10:32 -0700

    Fixed bug in parser where each SQL statement was wrongly assumed to have a single html doc, also created a class for the parser functions and added code to collect some counters of the data

--- src/parse_sql.py
+++ src/parse_sql.py
@@ -38,167 +38,216 @@ class InfoBoxHTMLParser(HTMLParser):
     def get_info(self):
         return self.info
 
-def add_workout_to_user(user_id, data_dict, info_dict, outfolder):
-    # creates a file for the user (if not already exists) and adds a workout in JSON format
-    filepath = os.path.join(outfolder, str(user_id) + ".json")
-    
-    # Create ONE dictionary of all information and data of ONE workout done by one user
-    workout_dict = info_dict    
-    if (data_dict.has_key('data')):
-        workout_dict['data'] = data_dict['data']
-    
-    # add to user's file
-    duplicate = False
-    if (os.path.isfile(filepath)):  # if user's file already exists, read the full thin and add the new workout
-        # read all workouts from user's file
-        f = open(filepath)
-        j = json.load(f)
-        f.close()
+
+class SqlToJsonData(object):
+
+    def __init__(self, sqlfile, max_users = -1, max_workouts = -1):
+        self.sqlfile = sqlfile
+        self.workouts_without_user = 0
+        self.duplicate_workouts = 0
+        self.lines_parsed = 0
+        self.users = 0
+        self.workouts = 0
+        self.workouts_without_data = 0
+        self.workouts_without_info = 0
+        self.max_users = max_users
+        self.max_workouts = max_workouts
+
+    def add_workout_to_user(self, user_id, data_dict, info_dict, outfolder):
+        # creates a file for the user (if not already exists) and adds a workout in JSON format
+        filepath = os.path.join(outfolder, str(user_id) + ".json")
         
-        # check if this workout is duplicate
-        workouts = j['workouts']
-        for w in workouts:
-            if (str(workout_dict) == str(w)):
-                duplicate = True
-                print "Detected duplicate !"
-                return
+        # Create ONE dictionary of all information and data of ONE workout done by one user
+        workout_dict = info_dict    
+        if (data_dict.has_key('data')):
+            workout_dict['data'] = data_dict['data']
         
-        j['workouts'].append(workout_dict)  # add a dict to the list of dicts
-    else:           # else just create a new workout
-        j = {}
-        j['id'] = str(user_id)
-        j['workouts'] = [workout_dict]
-   
-    # write back everything to the file
-    print "Writing to " + filepath
-    f = open(filepath, "w") # not a very efficient way of adding something to a file, but okay for now
-    json.dump(j, f)
-    f.close()
-
-def extract_user_id(html):
-    # extract user id
-    start = html.find("/workouts/user/")
-    if (start == -1):
-        raise Exception("illegal input to this parse_user_event()..")
-    start = start + len("/workouts/user/")
-    end = html.find("\\\"", start)
-    user_id = int(html[start:end])
-    return user_id
-    #print "user id = " + str(user_id)
-
-
-def extract_trace_data(html):
-    # extract info from 'data' - these are the traces (gps, heart-rate, pace)
-    json_data = {}
-    json_string = ""
-    start = html.find("\"data\\\"")
-    if (start != -1):
-        end = html.find("]", start)
-        json_string = "{" + html[start : end + 1] + "}"
-        json_string = re.sub(r'\\n',r'',json_string)
-        json_string = re.sub(r'\\"',r'"',json_string)
-        json_data = json.loads(json_string)
-    return json_data
-
-def extract_info_box(html):
-    # extract info box - hydration, wind etc.
-    # look for the string "<div class="tab-panel">" and then find the end tag
-    info_data = {}
-    start = html.find("<div class=\\\"tab-panel\\\">")
-    if (start != -1):
-        #print "found tab panel at index " + str(start)
-        start = html.find("<ul class=\\\"summary clearfix\\\">", start + 1)
-        #print "found ul element at " + str(start)
-        end = html.find("</ul>", start + 1)
-        info_box_string = html[start : end + len("</ul>")]
-        info_box_string = re.sub(r'\\n',r'', info_box_string)
-        info_parser = InfoBoxHTMLParser()
-        info_parser.feed(info_box_string)
-        info_data = info_parser.get_info() # dictionary of all information in info box
-    return info_data
-
-
-def extract_date_time(html):
-    # extract date and time
-    start = html.find("<div class=\\\"date-time\\\">")
-    date_time_string = None
-    if (start != -1):
-        #print "Found date-time.."
-        end = html.find("</div>", start + 1)
-        date_time_string = html[start + len("<div class=\\\"date-time\\\">") : end]
-    return date_time_string
-
-def parse_user_event(html, outfolder):
-    # html string contains ONE user id, all trace data and info box data
-    
-    # extract user id
-    user_id = extract_user_id(html)
-    
-    # extract info from 'data' - these are the traces (gps, heart-rate, pace)
-    trace_data = extract_trace_data(html)
-    
-    # extract info box - hydration, wind etc.
-    info_data = extract_info_box(html)
-    
-    # extract date and time string
-    date_time_string = extract_date_time(html)
-    if (date_time_string is not None):
-        info_data['date-time'] = date_time_string
+        # add to user's file
+        duplicate = False
+        if (os.path.isfile(filepath)):  # if user's file already exists, read the full thin and add the new workout
+            # read all workouts from user's file
+            f = open(filepath)
+            j = json.load(f)
+            f.close()
             
-    # add to user's file
-    add_workout_to_user(user_id, trace_data, info_data, outfolder)
+            # check if this workout is duplicate
+            workouts = j['workouts']
+            for w in workouts:
+                if (str(workout_dict) == str(w)):
+                    duplicate = True
+                    self.duplicate_workouts += 1
+                    return
+            
+            j['workouts'].append(workout_dict)  # add a dict to the list of dicts
+            print "User "+ str(user_id) + " has more than 1 workout.."
+        else:           # else just create a new workout
+            self.users += 1
+            j = {}
+            j['id'] = str(user_id)
+            j['workouts'] = [workout_dict]
+       
+        self.workouts += 1
 
-def parse_html(workout_id, html, outfolder):
-    user_start = html.find("/workouts/user")
-    if (user_start == -1):
-        raise Exception("No user found..")
-    while(user_start < len(html)):
-        user_end = html.find("/workouts/user", user_start + 1)
-        if (user_end == -1):
-            user_end = len(html)
-        user_string = html[user_start:user_end]
-        user_start = user_end
-        parse_user_event(user_string, outfolder)
-        #break   # for testing, restrict one user
+        # write back everything to the file
+        print "Writing to " + filepath
+        f = open(filepath, "w") # not a very efficient way of adding something to a file, but okay for now
+        json.dump(j, f)
+        f.close()
 
-def parse_sql_file(infile):
-    if (not os.path.isfile(infile)):
-        print "File not found.."
-        exit(0)
+    def extract_user_id(self, html):
+        # extract user id
+        start = html.find("/workouts/user/")
+        if (start == -1):
+            raise Exception("illegal input to this parse_user_event()..")
+        start = start + len("/workouts/user/")
+        end = html.find("\\\"", start)
+        user_id = int(html[start:end])
+        if (html.find("/workouts/user/", start) != -1):
+            raise Exception("Multiple user ids in one html doc")
+        return user_id
+        #print "user id = " + str(user_id)
 
-    print "Reading file " + infile
 
-    # create output folder name
-    infile_name, infile_ext = os.path.splitext(infile);
-    infile_name = os.path.basename(infile_name)
-    outfolder = os.path.join(os.path.dirname(__file__),"..","data",infile_name)
-    if (os.path.isdir(outfolder)):
-        shutil.rmtree(outfolder)
-        print "Removed existing folder " + outfolder
-    os.mkdir(outfolder)
-    print "Created folder " + outfolder
+    def extract_trace_data(self, html):
+        # extract info from 'data' - these are the traces (gps, heart-rate, pace)
+        json_data = {}
+        json_string = ""
+        start = html.find("\"data\\\"")
+        if (start != -1):
+            end = html.find("]", start)
+            json_string = "{" + html[start : end + 1] + "}"
+            json_string = re.sub(r'\\n',r'',json_string)
+            json_string = re.sub(r'\\"',r'"',json_string)
+            json_data = json.loads(json_string)
+        else:
+            self.workouts_without_data += 1
+        if (html.find("\"data\\\"", start + 1) != -1):
+            raise Exception("Multiple 'data' elements in one html doc")
+        return json_data
 
-    # now read input file
-    with open(infile) as f:
-        workout_id = ""
-        html = ""
-        n_records = 0
-        for line in f:
-            if ("INSERT INTO `EndoMondoWorkouts` VALUES" in line):  # ignore all other lines
-                record = line.split("INSERT INTO `EndoMondoWorkouts` VALUES ")[1].strip()
-                comma_index = record.find(',')  # separate workout_id and html string
-                workout_id = record[1:comma_index]
-                print "workout_id = " + workout_id
-                html = record[comma_index + 1:]
-                parse_html(workout_id, html, outfolder)    # this will extract relevant data
+    def extract_info_box(self, html):
+        # extract info box - hydration, wind etc.
+        # look for the string "<div class="tab-panel">" and then find the end tag
+        info_data = {}
+        start = html.find("<div class=\\\"tab-panel\\\">")
+        if (start != -1):
+            #print "found tab panel at index " + str(start)
+            start = html.find("<ul class=\\\"summary clearfix\\\">", start + 1)
+            #print "found ul element at " + str(start)
+            end = html.find("</ul>", start + 1)
+            info_box_string = html[start : end + len("</ul>")]
+            info_box_string = re.sub(r'\\n',r'', info_box_string)
+            info_parser = InfoBoxHTMLParser()
+            info_parser.feed(info_box_string)
+            info_data = info_parser.get_info() # dictionary of all information in info box
+        else:
+            self.workouts_without_info += 1
+        if (html.find("<div class=\\\"tab-panel\\\">", start + 1) != -1):
+            raise Exception("Multiple tab-panels in one html doc")
+        return info_data
 
-                #f2 = open("test_html.html",'w')
-                #f2.write(html)
-                #f2.close()
 
-                n_records = n_records + 1
-                if (n_records == 1):
+    def extract_date_time(self, html):
+        # extract date and time
+        start = html.find("<div class=\\\"date-time\\\">")
+        date_time_string = None
+        if (start != -1):
+            #print "Found date-time.."
+            end = html.find("</div>", start + 1)
+            date_time_string = html[start + len("<div class=\\\"date-time\\\">") : end]
+        if (html.find("<div class=\\\"date-time\\\">", start + 1) != -1):
+            raise Exception("Multiple date-time elements in one html")
+        return date_time_string
+
+    def parse_user_event(self, html, outfolder):
+        # html string contains ONE user id, all trace data and info box data
+        
+        # extract user id
+        user_id = self.extract_user_id(html)
+        
+        # extract info from 'data' - these are the traces (gps, heart-rate, pace)
+        trace_data = self.extract_trace_data(html)
+        
+        # extract info box - hydration, wind etc.
+        info_data = self.extract_info_box(html)
+        
+        # extract date and time string
+        date_time_string = self.extract_date_time(html)
+        if (date_time_string is not None):
+            info_data['date-time'] = date_time_string
+                
+        # add to user's file
+        self.add_workout_to_user(user_id, trace_data, info_data, outfolder)
+
+    def parse_html(self, workout_id, html, outfolder):
+        print "Processing workout " + workout_id
+        start = html.find("/workouts/user")
+        if (start == -1):
+            print "\tNo user found..."
+            self.workouts_without_user += 1
+            return
+        if (html.find("/workouts/user", start + 1) != -1):
+            raise Exception("Multiple users found in one html string..")
+        self.parse_user_event(html, outfolder)
+
+    def print_stats(self):
+        print "# lines parsed = ", self.lines_parsed
+        print "# duplicate workouts = ", self.duplicate_workouts
+        print "# workouts_without_user = ", self.workouts_without_user
+        print "# workouts successfully extracted = ", self.workouts
+        print "# users = ", self.users
+        print "# workouts without trace data = ", self.workouts_without_data
+        print "# workouts without info box = ", self.workouts_without_info
+
+    def done(self):
+        if (self.max_users > 0 and self.users >= self.max_users):
+            return True
+        if (self.max_workouts > 0 and self.workouts >= self.max_workouts):
+            return True
+        return False
+
+    def run(self):
+        infile = self.sqlfile
+        if (not os.path.isfile(infile)):
+            print "File not found.."
+            exit(0)
+
+        print "Reading file " + infile
+
+        # create output folder name
+        infile_name, infile_ext = os.path.splitext(infile);
+        infile_name = os.path.basename(infile_name)
+        outfolder = os.path.join(os.path.dirname(__file__),"..","data",infile_name)
+        if (os.path.isdir(outfolder)):
+            shutil.rmtree(outfolder)
+            print "Removed existing folder " + outfolder
+        os.mkdir(outfolder)
+        print "Created folder " + outfolder
+
+        # now read input file
+        with open(infile) as f:
+            workout_id = ""
+            html = ""
+            n_records = 0
+            for line in f:
+                if (self.done()):
                     break
+                if ("INSERT INTO `EndoMondoWorkouts` VALUES" in line):  # ignore all other lines
+                    self.lines_parsed += 1
+                    start = line.find("(")
+                    while(start != -1 and (not self.done())):
+                            p2 = line.find("<!DOCTYPE html", start + 1)
+                            workout_id = line[start + 1 : p2 - 2]
+                            start = p2
+                            end = line.find("</html>", start + 1)
+                            html = line[start : end + len("</html>")]
+                            self.parse_html(workout_id, html, outfolder)
+                            start = line.find("(", end + 1)
+                            #f2 = open("test_html.html",'w')
+                            #f2.write(html)
+                            #f2.close()
+        
+        self.print_stats()
 
 
 if __name__ == "__main__":
@@ -206,4 +255,5 @@ if __name__ == "__main__":
         print "Usage: parse_csv.py <file>"
         exit(0)
     infile = sys.argv[1]
-    parse_sql_file(infile)
+    s = SqlToJsonData(infile, max_users=100, max_workouts=100)
+    s.run()
