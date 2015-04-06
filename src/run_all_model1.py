a8759a89c93a46ce738c9cc98fe0e3c2a418a59b
yashkarandikar <yashkarandikar@gmail.com>
2015-04-05 23:57:11 -0700

    adding run scripts for model1 and model2

---
+++ src/run_all_model1.py
@@ -0,0 +1,17 @@
+import os
+import time
+
+t1 = time.time()
+lam1 = 0.0
+while (lam1 <= 10.0):
+    ofile = "runs_model1/random/output_%f.txt" % (lam1)
+    command = "python predictor_duration_user.py %f > %s" % (lam1, ofile)
+    print "Running : ", command
+    os.system(command)
+    if (lam1 == 0.0):
+        lam1 = 0.0001
+    else:
+        lam1 = lam1 * 10.0
+print "Done"
+t2 = time.time()
+print "Total time taken  = ", t2 - t1
---
+++ src/run_all_model2.py
@@ -0,0 +1,24 @@
+import os
+import time
+
+#os.system("python predictor_duration_evolving_user.py 0.0 0.0 > output_0_0.txt")
+t1 = time.time()
+lam1 = 0.0
+while (lam1 <= 1.0):
+    lam2 = 0.0
+    while (lam2 <= 1.0):
+        ofile = "runs_evolving_model/output_%f_%f.txt" % (lam1, lam2)
+        command = "python predictor_duration_evolving_user.py %f %f > %s" % (lam1, lam2, ofile)
+        print "Running : ", command
+        os.system(command)
+        if (lam2 == 0.0):
+            lam2 = 0.001
+        else:
+            lam2 = lam2 * 10.0
+    if (lam1 == 0.0):
+        lam1 = 0.001
+    else:
+        lam1 = lam1 * 10.0
+print "Done"
+t2 = time.time()
+print "Total time taken  = ", t2 - t1
