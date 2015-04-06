1879023b10aef45a130c82784cc9326dbf16e9e8
yashkarandikar <yashkarandikar@gmail.com>
2015-04-06 09:08:58 -0700

    renamed reg plot script and added reg plot script for model 1

--- src/reg_plot_model1.py
+++ src/reg_plot_model1.py
@@ -5,10 +5,10 @@ def main():
     files = sys.argv[1:]
     train = {}
     val = {}
-    found = {'train_r2' : False, 'val_r2' : False, 'lam1' : False, 'lam2' : False}
+    found = {'train_r2' : False, 'val_r2' : False, 'lam1' : False}
     for fi in files:
         with open(fi) as f:
-            train_r2 = 0.0; val_r2 = 0.0; lam1 = 0.0; lam2 = 0.0
+            train_r2 = 0.0; val_r2 = 0.0; lam1 = 0.0
             for k in found.keys():
                 found[k] = False
             for line in f:
@@ -20,7 +20,8 @@ def main():
                     elif (line.startswith("Validation")):
                         val_r2 = float(line.split("=")[-1].strip())
                         found['val_r2'] = True
-                    if ("lam1" in line and "lam2" in line):
+                    if ("lam1" in line):
+                        print line
                         parts = line.split(",")
                         for p in parts:
                             k, v = p.split("=")
@@ -29,51 +30,60 @@ def main():
                             if (k == "lam1"):
                                 lam1 = float(v)
                                 found['lam1'] = True
-                            if (k == "lam2"):
-                                lam2 = float(v)
-                                found['lam2'] = True
             found_all = True
             for v in found.values():
                 if (not v):
                     found_all = False
                     break
             if (found_all):
-                if (not train.has_key(lam1)):
-                    train[lam1] = {}
-                if (not val.has_key(lam1)):
-                    val[lam1] = {}
-                train[lam1][lam2] = train_r2
-                val[lam1][lam2] = val_r2
+                if (lam1 == 0):
+                    lam1 = 1e-12;
+                train[lam1] = train_r2
+                val[lam1] = val_r2
 
     print train
     print val
 
     max_r2 = 0.0
     max_lam1 = 0.0
-    max_lam2 = 0.0
     plt.figure()
-    for k1, v1 in val.items():
+    
+    lam_list = []
+    train_r2_list = []
+    for k1, v1 in sorted(train.items()):
         lam1 = k1
-        lam2 = []; r2 = []
-        for k2, v2 in v1.items():
-            lam2.append(k2)
-            r2.append(v2)
-            if (v2 > max_r2):
-                max_r2 = v2
-                max_lam1 = lam1
-                max_lam2 = k2
+        r2 = v1
+        lam_list.append(lam1)
+        train_r2_list.append(r2)
+    both = zip(lam_list, train_r2_list)
+    both.sort()
+    lam_list = [a for a,b in both]
+    train_r2_list = [b for a,b in both]
+    plt.plot(lam_list, train_r2_list, label = "Training", marker = "o")
+    
+    lam_list = []
+    val_r2_list = []
+    for k1, v1 in sorted(val.items()):
+        lam1 = k1
+        r2 = v1
+        lam_list.append(lam1)
+        val_r2_list.append(r2)
+        if (r2 > max_r2):
+            max_r2 = r2
+            max_lam1 = lam1
 
-        both = zip(lam2, r2)
-        both.sort()
-        lam2 = [a for a,b in both]
-        r2 = [b for a,b in both]
-        print lam2, r2
-        plt.plot(lam2, r2, label = "Lam1 = %f" % (lam1))
-    print "Best R2 = %f at lam1 = %f and lam2 = %f" % (max_r2, max_lam1, max_lam2)
-    plt.xlabel("lam2")
+    both = zip(lam_list, val_r2_list)
+    both.sort()
+    lam_list = [a for a,b in both]
+    val_r2_list = [b for a,b in both]
+    plt.plot(lam_list, val_r2_list, label = "Validation", marker = "o")
+    print "Best Validation R2 = %f is at lam1 = %f" % (max_r2, max_lam1)
+    print "Training R2 at lam1 = %f is %f" % (max_lam1, train[max_lam1])
+    plt.xlabel("lambda")
     plt.ylabel("R2 on validation set")
-    plt.legend()
+    plt.legend(loc = "best")
     plt.semilogx(basex=10)
+    #plt.xscale('symlog')
     plt.show()
 
 if __name__ == "__main__":
