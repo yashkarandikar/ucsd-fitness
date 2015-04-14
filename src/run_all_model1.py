b3be245931ef47ce06d10b49794a9c28156b6993
yashkarandikar <yashkarandikar@gmail.com>
2015-04-13 17:56:52 -0700

    updated running scripts and todo

--- src/run_all_model1.py
+++ src/run_all_model1.py
@@ -4,7 +4,7 @@ import time
 t1 = time.time()
 lam1 = 0.0
 while (lam1 <= 10.0):
-    ofile = "runs_model1/random/output_%f.txt" % (lam1)
+    ofile = "runs_model1/final_20/output_%f.txt" % (lam1)
     command = "python predictor_duration_user.py %f > %s" % (lam1, ofile)
     print "Running : ", command
     os.system(command)
