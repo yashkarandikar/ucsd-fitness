a5e94bc504e20f610e2396c3295c5d0547625c52
yashkarandikar <yashkarandikar@gmail.com>
2015-04-26 12:33:19 -0700

    optimized fit_tiredness_for_all_workouts_pyx by converting hr to np data type and using xrange

--- src/predictor_many_insthr_evolving_pyx.pyx
+++ src/predictor_many_insthr_evolving_pyx.pyx
@@ -55,7 +55,7 @@ def find_best_path_DP(np.ndarray[DTYPE_t, ndim=2] M):
 
     return [leastError, path]
 
-def fit_tiredness_for_all_workouts_pyx(np.ndarray[DTYPE_t, ndim=1] theta, np.ndarray[DTYPE_t, ndim=2] data, np.int_t E, sigma, hr = None, np.ndarray[long, ndim=1] last_e = None):
+def fit_tiredness_for_all_workouts_pyx(np.ndarray[DTYPE_t, ndim=1] theta, np.ndarray[DTYPE_t, ndim=2] data, np.int_t E, sigma, np.ndarray[DTYPE_t, ndim=2] hr = None, np.ndarray[long, ndim=1] last_e = None):
     from predictor_many_insthr_evolving import get_theta_0, get_theta_1, get_alpha_e, get_alpha_ue, get_workout_count
     # sigma - set of experience levels for all workouts for all users.. sigma is a matrix.. sigma(u,i) = e_ui i.e experience level of user u at workout i - these values are NOT optimized by L-BFGS.. they are optimized by DP procedure
     cdef int U = get_workout_count(data)
@@ -67,7 +67,7 @@ def fit_tiredness_for_all_workouts_pyx(np.ndarray[DTYPE_t, ndim=1] theta, np.nda
     cdef np.ndarray[DTYPE_t, ndim=2] M
     cdef int n_skipped = 0
     changed = False
-    for u in range(0, U):
+    for u in xrange(0, U):
         Nu = 0
         row_u = row
         while (row < N and data[row, 0] == u):
@@ -83,13 +83,13 @@ def fit_tiredness_for_all_workouts_pyx(np.ndarray[DTYPE_t, ndim=1] theta, np.nda
         if (n_E > 1):
             # populate M
             M = np.zeros((n_E, Nu))
-            for j in range(0, Nu):  # over all workouts for this user
+            for j in xrange(0, Nu):  # over all workouts for this user
                 if (hr is None):
                     t = data[row_u + j, 3]    # actual time for that workout
                 else:
-                    t = hr[row_u + j]
+                    t = hr[row_u + j, 0]
                 d = data[row_u + j, 2]
-                for i in range(low_E, E):       # over all experience levels
+                for i in xrange(low_E, E):       # over all experience levels
                     a_ue = get_alpha_ue(theta, u, i, E)[0]
                     a_e = get_alpha_e(theta, i, E, U)[0]
                     tprime = (a_e + a_ue) * (theta_0 + theta_1 * d)
@@ -101,7 +101,7 @@ def fit_tiredness_for_all_workouts_pyx(np.ndarray[DTYPE_t, ndim=1] theta, np.nda
             bestPath = [e + low_E for e in bestPath]
             #print minError, bestPath
             # update sigma matrix using bestPath
-            for i in range(0, Nu):
+            for i in xrange(0, Nu):
                 if (sigma[u][i] != bestPath[i]):
                     sigma[u][i] = bestPath[i]
                     changed = True
