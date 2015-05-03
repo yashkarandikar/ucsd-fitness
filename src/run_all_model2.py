5509fd9325991fcac533c735e2371b398bc43d97
yashkarandikar <yashkarandikar@gmail.com>
2015-05-02 23:02:02 -0700

    avg hr random E 2 threshold 10

--- src/run_all_model2.py
+++ src/run_all_model2.py
@@ -7,7 +7,7 @@ lam1 = 0.0
 while (lam1 <= 1.0):
     lam2 = 0.0
     while (lam2 <= 1.0):
-        ofile = "runs_evolving_model_hr/final_E2_10/output_%f_%f.txt" % (lam1, lam2)
+        ofile = "runs_evolving_model_hr/random_E2_10/output_%f_%f.txt" % (lam1, lam2)
         command = "python predictor_avghr_evolving_user.py %f %f > %s" % (lam1, lam2, ofile)
         print "Running : ", command
         os.system(command)
