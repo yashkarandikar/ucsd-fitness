4dd7d94a37f4c57dfdc1740f6a05bd2788fe786e
yashkarandikar <yashkarandikar@gmail.com>
2015-05-05 11:15:49 -0700

    duration random test runs

--- src/predictor_duration_evolving_user.py
+++ src/predictor_duration_evolving_user.py
@@ -852,11 +852,12 @@ if __name__ == "__main__":
     data = np.load(outfile)
     train_set = data["train_set"]
     val_set = data["val_set"]
-    #test_set = data["test_set"]
+    test_set = data["test_set"]
     param_indices = data["param_indices"][()]
     print "Doing sorted check on train and val sets.."
     check_sorted(train_set, param_indices)
     check_sorted(val_set, param_indices)
+    check_sorted(test_set, param_indices)
 
     print "Number of users = ", get_user_count(train_set)
     print "Training set has %d examples" % (train_set.shape[0])
@@ -879,12 +880,12 @@ if __name__ == "__main__":
     print "Adding experience levels to data matrices"
     train_set = add_experience_column_to_train_set(train_set, sigma, param_indices)
     val_set = add_experience_column_to_test_set(val_set, train_set, param_indices, mode = mode)
-    #test_set = add_experience_column_to_test_set(test_set, train_set, param_indices, mode = mode)
+    test_set = add_experience_column_to_test_set(test_set, train_set, param_indices, mode = mode)
 
     print "Making predictions.."
     train_pred = make_predictions(train_set, theta, E, param_indices)
     val_pred = make_predictions(val_set, theta, E, param_indices)
-    #test_pred = make_predictions(test_set, theta, E, param_indices)
+    test_pred = make_predictions(test_set, theta, E, param_indices)
     print param_indices
 
     print "Computing statistics"
@@ -895,8 +896,8 @@ if __name__ == "__main__":
     print "\n@Training Examples = %d,MSE = %f,Variance = %f,FVU = %f,R2 = 1 - FVU = %f\n" % (train_set.shape[0],mse, var, fvu, r2)
     [mse, var, fvu, r2, errors] = compute_stats(val_set[:, param_indices["Duration"]], val_pred)
     print "@Validation Examples = %d,MSE = %f,Variance = %f,FVU = %f,R2 = 1 - FVU = %f\n" % (val_set.shape[0],mse, var, fvu, r2)
-    #[mse, var, fvu, r2, errors] = compute_stats(test_set[:, param_indices["Duration"]], test_pred)
-    #print "@Test Examples = %d,MSE = %f,Variance = %f,FVU = %f,R2 = 1 - FVU = %f\n" % (test_set.shape[0],mse, var, fvu, r2)
+    [mse, var, fvu, r2, errors] = compute_stats(test_set[:, param_indices["Duration"]], test_pred)
+    print "@Test Examples = %d,MSE = %f,Variance = %f,FVU = %f,R2 = 1 - FVU = %f\n" % (test_set.shape[0],mse, var, fvu, r2)
 
     t2 = time.time()
     print "@Total time taken = ", t2 - t1
