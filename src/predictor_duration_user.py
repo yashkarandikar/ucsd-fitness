3eb616dbe7324b1242246b287c6e8fe48912c64e
yashkarandikar <yashkarandikar@gmail.com>
2015-05-05 11:04:41 -0700

    duration random E1 10

--- src/predictor_duration_user.py
+++ src/predictor_duration_user.py
@@ -367,7 +367,7 @@ if __name__ == "__main__":
     #infile = "../../data/all_workouts_train_and_val_condensed.gz"
     infile = "../../data/all_workouts_condensed.gz"
     #infile = "synth_user_model.gz"
-    mode = "final"
+    mode = "random"
     outfile = infile + mode + ".npz"
     #outfile = infile + ".npz"
     e_fn = E_pyx
