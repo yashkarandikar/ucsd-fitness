1719d2475256a94aab376d98f489e71e6c72e396
yashkarandikar <yashkarandikar@gmail.com>
2015-05-06 13:54:17 -0700

    time series runs

--- src/run_all_timeseries.py
+++ src/run_all_timeseries.py
@@ -2,12 +2,14 @@ import time
 import os
 
 t1 = time.time()
-E = [50, 1, 10, 20, 30, 40]
-lam = 10000.0
-output_dir = "runs_timeseries/new/E"
+#E = [50, 1, 10, 20, 30, 40]
+E = [30]
+lam = [1000.0, 10000.0, 100000.0]
+output_dir = "runs_timeseries/new/E/30"
 for e in E:
-    ofile = output_dir + "/" + "output_" + str(e) + ".txt"
-    command = "python timeseries_linear.py %f %d > %s" % (lam, e, ofile)
-    print "Running : ", command
-    os.system(command)
+    for l in lam:
+        ofile = output_dir + "/" + "output_" + str(l) + "_" + str(e) + ".txt"
+        command = "python timeseries_linear.py %f %d > %s" % (l, e, ofile)
+        print "Running : ", command
+        os.system(command)
 t2 = time.time()
