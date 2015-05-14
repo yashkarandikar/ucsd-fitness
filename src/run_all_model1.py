9627534f3136902c9af1f384c632c93dc1152d3b
yashkarandikar <yashkarandikar@gmail.com>
2015-05-14 09:42:43 -0700

    run scripts

--- src/run_all_model1.py
+++ src/run_all_model1.py
@@ -1,12 +1,24 @@
 import os
 import time
 
+l1 = [0.0, 0.0001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0]
+t1 = time.time()
+for lam1 in l1:
+    if (lam1 >= 100.0):
+        ofile = "runs_model1_hr/random_E1_10/output_%f.txt" % (lam1)
+        command = "python predictor_avghr_user.py %f > %s" % (lam1, ofile)
+        print "Running : ", command
+        os.system(command)
+t2 = time.time()
+print "Total time taken  = ", t2 - t1
+
+"""
 t1 = time.time()
 lam1 = 0.0
 while (lam1 <= 10.0):
     #ofile = "runs_model1/final_20/output_%f.txt" % (lam1)
-    ofile = "runs_model1/random_E1_10/output_%f.txt" % (lam1)
-    command = "python predictor_duration_user.py %f > %s" % (lam1, ofile)
+    ofile = "runs_model1_hr/random_E1_10/output_%f.txt" % (lam1)
+    command = "python predictor_avghr_user.py %f > %s" % (lam1, ofile)
     print "Running : ", command
     os.system(command)
     if (lam1 == 0.0):
@@ -16,3 +28,4 @@ while (lam1 <= 10.0):
 print "Done"
 t2 = time.time()
 print "Total time taken  = ", t2 - t1
+"""
--- src/run_all_model2.py
+++ src/run_all_model2.py
@@ -1,14 +1,28 @@
 import os
 import time
 
+l1 = [0.0, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0]
+l2 = [0.0, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0]
+t1 = time.time()
+for lam1 in l1:
+    for lam2 in l2:
+        if (lam1 >= 10000.0 or lam2 >= 10000.0):
+            ofile = "runs_evolving_model_hr/random_E3_10/output_%f_%f.txt" % (lam1, lam2)
+            command = "python predictor_avghr_evolving_user.py %f %f > %s" % (lam1, lam2, ofile)
+            print "Running : ", command
+            os.system(command)
+t2 = time.time()
+print "Total time taken  = ", t2 - t1
+
 #os.system("python predictor_duration_evolving_user.py 0.0 0.0 > output_0_0.txt")
+"""
 t1 = time.time()
 lam1 = 0.0
-while (lam1 <= 1.0):
+while (lam1 <= 10.0):
     lam2 = 0.0
-    while (lam2 <= 1.0):
-        ofile = "runs_evolving_model/random_E2_10/output_%f_%f.txt" % (lam1, lam2)
-        command = "python predictor_duration_evolving_user.py %f %f > %s" % (lam1, lam2, ofile)
+    while (lam2 <= 10.0):
+        ofile = "runs_evolving_model_hr/final_E3_10/output_%f_%f.txt" % (lam1, lam2)
+        command = "python predictor_avghr_evolving_user.py %f %f > %s" % (lam1, lam2, ofile)
         print "Running : ", command
         os.system(command)
         if (lam2 == 0.0):
@@ -22,3 +36,4 @@ while (lam1 <= 1.0):
 print "Done"
 t2 = time.time()
 print "Total time taken  = ", t2 - t1
+"""
