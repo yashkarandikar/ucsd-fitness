045eb5b9bd5e7e2c5a4309e51682591867706b3a
yashkarandikar <yashkarandikar@gmail.com>
2015-05-14 23:01:29 -0700

    run all model2 new script

--- src/run_all_model2.py
+++ src/run_all_model2.py
@@ -1,14 +1,16 @@
 import os
 import time
 
-l1 = [0.0, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0]
-l2 = [0.0, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0]
+#l1 = [0.0, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0]
+#l2 = [0.0, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0]
+l1 = [0.0, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+l2 = [0.0, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
 t1 = time.time()
 for lam1 in l1:
     for lam2 in l2:
-        if (lam1 >= 10000.0 or lam2 >= 10000.0):
-            ofile = "runs_evolving_model_hr/random_E3_10/output_%f_%f.txt" % (lam1, lam2)
-            command = "python predictor_avghr_evolving_user.py %f %f > %s" % (lam1, lam2, ofile)
+        if (lam1 >= 100.0 or lam2 >= 100.0):
+            ofile = "runs_evolving_model/final_E3_10/output_%f_%f.txt" % (lam1, lam2)
+            command = "python predictor_duration_evolving_user.py %f %f > %s" % (lam1, lam2, ofile)
             print "Running : ", command
             os.system(command)
 t2 = time.time()
