2d07deaf50b7a340b547267fcaa07e93e8092639
yashkarandikar <yashkarandikar@gmail.com>
2015-05-14 23:00:11 -0700

    run all insthr new script

--- src/run_all_insthr.py
+++ src/run_all_insthr.py
@@ -1,6 +1,20 @@
 import os
 import time
 
+l1 = [0.0, 0.001, 0.01, 0.1, 1.0]
+l2 = [0.0, 0.001, 0.01, 0.1, 1.0]
+t1 = time.time()
+for lam1 in l1:
+    for lam2 in l2:
+        #if (lam1 == 10.0 or lam2 == 10.0):
+        ofile = "runs_model2_inst/final_E10/output_%f_%f.txt" % (lam1, lam2)
+        command = "python predictor_insthr_evolving.py %f %f 10 > %s" % (lam1, lam2, ofile)
+        print "Running : ", command
+        os.system(command)
+t2 = time.time()
+print "Total time taken  = ", t2 - t1
+
+"""
 t1 = time.time()
 lam1 = 0.0
 while (lam1 <= 1.0):
@@ -21,3 +35,4 @@ while (lam1 <= 1.0):
 print "Done"
 t2 = time.time()
 print "Total time taken  = ", t2 - t1
+"""
